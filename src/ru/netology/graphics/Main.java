132679c7fa90601e430af6678b727acadbdb0fbd
Natalia Zubareva <n.zubareva@inbox.ru>
2022-02-27 15:10:05 +0300

    Main class fixed

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -12,13 +12,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         //TextGraphicsConverter converter = null; // Создайте тут объект вашего класса конвертера
         TextGraphicsConverter converter = new ImageConverter();
-        GServer server = new GServer(converter); // Создаём объект сервера
-        server.start(); // Запускаем
+//        GServer server = new GServer(converter); // Создаём объект сервера
+//        server.start(); // Запускаем
 
         // Или то же, но с выводом на экран:
-//        String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
-//        String imgTxt = converter.convert(url);
-//        System.out.println(imgTxt);
+        String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
+        String imgTxt = converter.convert(url);
+        System.out.println(imgTxt);
 
     }
 
